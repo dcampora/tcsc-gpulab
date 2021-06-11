39c486096837a8dd6ce8d8d7f6efceaeb92ce909
Dorothea vom Bruch <dorothea.vom.bruch@cern.ch>
2021-06-11 17:26:34 +0200

    update vector_addition exercise

--- vector_addition/vector_addition.cu
+++ vector_addition/vector_addition.cu
@@ -5,92 +5,126 @@
 
    author: Dorothea vom Bruch (dorothea.vom.bruch@cern.ch)
    date: 05/2019
+   updated: 06/2021
 
  */
 
 #include <stdio.h>
 #include <iostream>
-#include <chrono>
-
-#include "../helpers/helpers.h"
 
 using namespace std;
 
-__constant__ int vec_size_d;
+void init_with(int* a, float val, int N) {
+    for ( int i = 0; i < N; i++ ) {
+        a[i] = val;
+    }
+}
 
-__global__ void vector_addition_kernel( int *a, int *b, int *c) {
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
+void vector_addition_cpu(int *a, int *b, int *c, int N) {
+    for (int i = 0; i < N; i++) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+void check_elements(int correct, int* vec, int N) {
+    for (int i = 0; i < N; i++) {
+         if (vec[i] != correct) {
+                 printf("ERROR: vec[%u] = %d, should be %d \n", i, vec[i], correct);
+         }
+    }
+}
+
+/* Step 2 to do: Add the __global__ keyword in front of the function declaration  */
+void vector_addition_gpu(int *a, int *b, int *c, int size) {
   
-  if ( index < vec_size_d ) {
-    c[ index ] = a[ index ] + b[ index ];
+  /* Step 2 to do: modify the for loop such that it is executed in parallel
+     Follow the instructions in the notebook for details
+     The idea is to use threadIdx.x, blockIdx.x and blockDim.x when configuring the for loop 
+   */
+  for (int i = 0; i < size; i ++) { // <- change only this line!
+    c[i] = a[i] + b[i];
   }
-  
 }
 
 int main(int argc, char *argv[] ) {
 
-  if ( argc != 3 ) {
-    cout << "Need two arguments: size of vector and number of threads / block" << endl;
+  if ( argc != 4 ) {
+    cout << "Need three arguments: size of vector, number of threads / block and number of blocks in the grid" << endl;
     return -1;
   }
   
-  const int vec_size_h  = atoi(argv[argc-2]);
-  const int n_threads = atoi(argv[argc-1]);
+  const int size  = atoi(argv[argc-3]);
+  const int n_threads = atoi(argv[argc-2]);
+  const int n_blocks = atoi(argv[argc-1]);
+
+  cout << "Adding vectors of size " <<  size << " with " << n_threads << " threads" << " and " << n_blocks << " blocks" << endl;  
  
-  cout << "Adding vectors of size " <<  vec_size_h << " with " << n_threads << " threads" << endl;
-
   /* Host memory for the two input vectors a and b and the output vector c */
-  int *a_h = new int[vec_size_h];
-  int *b_h = new int[vec_size_h];
-  int *c_h = new int[vec_size_h];
+  int *a_h = new int[size];
+  int *b_h = new int[size];
+  int *c_h = new int[size];
 
-  for ( int i = 0; i < vec_size_h; i++ ) {
-    a_h[i] = i;
-    b_h[i] = i;
-    c_h[i] = 0;
-  }
+  /* Initialize vectors */
+  init_with(a_h, 13, size);
+  init_with(b_h, 9, size);
+  init_with(c_h, 0, size);
   
   /* Device pointers for the three vectors a, b, c */
   int *a_d, *b_d, *c_d;
-  CUDA_ASSERT( cudaMalloc( (void**)&a_d, vec_size_h * sizeof(int) ) );
-  CUDA_ASSERT( cudaMalloc( (void**)&b_d, vec_size_h * sizeof(int) ) );
-  CUDA_ASSERT( cudaMalloc( (void**)&c_d, vec_size_h * sizeof(int) ) );
-
-  /* Copy vectors to device */
-  CUDA_ASSERT( cudaMemcpy( a_d, a_h, vec_size_h * sizeof(int), cudaMemcpyHostToDevice ) );
-  CUDA_ASSERT( cudaMemcpy( b_d, b_h, vec_size_h * sizeof(int), cudaMemcpyHostToDevice ) );
   
-  CUDA_ASSERT( cudaMemcpyToSymbol( vec_size_d, &vec_size_h, sizeof(int) ) ); // copy to constant memory on GPU
+  /* Allocate device memory */
+  /* Step 1 to do: Allocate global memory for the device variables
+     Use the following syntax, which allocates size * sizeof(int) bytes and stores the pointer in *a_d:
+     cudaMalloc( (void**)&a_d, size * sizeof(int) );
+     Do the same for b_d and c_d!
+     */
+  
+  
+  /* Copy host vectors to the device */
+  /* Step 1 to do: Copy the host vectors to the device
+     Use the following syntax:
+     cudaMemcpy( a_d, a_h, size * sizeof(int), cudaMemcpyHostToDevice );
+     This copies size * sizeof(int) bytes from a_h to a_d, copying from the host to the device
+     Do the same to copy b_h to b_d and c_h to c_d
+     Note that we also copy vector c, such that it is initialized to zero. 
+     There other methods available in CUDA to do that, but for simplicity we do it this way.
+   */
 
+  
+  
   /* Define grid dimensions */
-  int n_blocks  = vec_size_h / n_threads + (vec_size_h % n_threads != 0);
-  dim3 blocks( n_blocks );
-  dim3 threads(n_threads);
+  /* Step 2 to do: define the dim3 variables for the grid dimensions as follows:
+     dim3 grid_dim(n_blocks);
+     dim3 block_dim(n_threads);
+  */
 
-  std::chrono::time_point<std::chrono::system_clock> start, end;
-  start = std::chrono::system_clock::now();
 
   /* Call kernel */
-  vector_addition_kernel<<<blocks,threads>>>( a_d, b_d, c_d);
+  /* Step 2 to do: Launch the kernel 
+     Call the kernel vector_addition_gpu with the grid dimension settings and passing the device vector pointers and the size as input
+   */
+  
+  
+  /* Copy result vector from device to host */
+  /* Step 3 to do: Copy content of c_d to c_h 
+     Use the following syntax:
+     cudaMemcpy( c_h, c_d, size * sizeof(int), cudaMemcpyDeviceToHost ); 
+   */
 
-  CUDA_ASSERT( cudaMemcpy( c_h, c_d, vec_size_h * sizeof(int), cudaMemcpyDeviceToHost ) );
-
+  
   /* Make sure GPU work is done */
-  cudaDeviceSynchronize();
+  /* Step 3 to do: Call the same synchronization function we used in the hello_world example to ensure that the GPU work has finished */
 
-  end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end-start;
+  /* Step 3 to do: Uncomment the call to check_elements to verify the vector addition result from the GPU  */
+  //check_elements(22, c_h, size);
+ 
+  /* Step 1 to do: Free device memory
+     Use the following syntax:
+     cudaFree( a_d );
+     This will free the global memory previously allocated with cudaMalloc
+     Do the same for b_d and c_d!
+   */
   
-  for ( int i = 0; i < vec_size_h; i++ ) {
-    cout << a_h[i] << " + " << b_h[i] << " = " << c_h[i] << endl;
-  }
-
-  cout << "Kernel duration: " << elapsed_seconds.count() << " s " << endl;
-  cout << "Time per kenel: " << elapsed_seconds.count() / vec_size_h << endl;
-  
-  CUDA_ASSERT( cudaFree( a_d ) );
-  CUDA_ASSERT( cudaFree( b_d ) );
-  CUDA_ASSERT( cudaFree( c_d ) );
 
   /* free host memory */
   delete [] a_h;
---
+++ vector_addition/vector_addition_solution.cu
@@ -0,0 +1,103 @@
+/**
+   Vector addition:
+   takes vectors a and b as input, computes vector sum 
+   and stores output in vector c
+
+   author: Dorothea vom Bruch (dorothea.vom.bruch@cern.ch)
+   date: 05/2019
+   updated: 06/2021
+
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void init_with(int* a, float val, int N) {
+    for ( int i = 0; i < N; i++ ) {
+        a[i] = val;
+    }
+}
+
+void vector_addition_cpu(int *a, int *b, int *c, int N) {
+    for (int i = 0; i < N; i++) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+void check_elements(int correct, int* vec, int N) {
+    for (int i = 0; i < N; i++) {
+         if (vec[i] != correct) {
+                 printf("ERROR: vec[%u] = %d, should be %d \n", i, vec[i], correct);
+         }
+    }
+}
+
+__global__ void vector_addition_gpu(int *a, int *b, int *c, int size) {
+  
+  for (int i = threadIdx.x; i < size; i += blockDim.x) {
+    c[i] = a[i] + b[i];
+  }
+}
+
+int main(int argc, char *argv[] ) {
+
+  if ( argc != 4 ) {
+    cout << "Need three arguments: size of vector, number of threads / block and number of blocks in the grid" << endl;
+    return -1;
+  }
+  
+  const int size  = atoi(argv[argc-3]);
+  const int n_threads = atoi(argv[argc-2]);
+  const int n_blocks = atoi(argv[argc-1]);
+
+  cout << "Adding vectors of size " <<  size << " with " << n_threads << " threads" << " and " << n_blocks << " blocks" << endl;  
+ 
+  /* Host memory for the two input vectors a and b and the output vector c */
+  int *a_h = new int[size];
+  int *b_h = new int[size];
+  int *c_h = new int[size];
+
+  /* Initialize vectors */
+  init_with(a_h, 13, size);
+  init_with(b_h, 9, size);
+  init_with(c_h, 0, size);
+  
+  /* Device pointers for the three vectors a, b, c */
+  int *a_d, *b_d, *c_d;
+  cudaMalloc( (void**)&a_d, size * sizeof(int) );
+  cudaMalloc( (void**)&b_d, size * sizeof(int) );
+  cudaMalloc( (void**)&c_d, size * sizeof(int) );
+
+  /* Copy vectors to device */
+  cudaMemcpy( a_d, a_h, size * sizeof(int), cudaMemcpyHostToDevice );
+  cudaMemcpy( b_d, b_h, size * sizeof(int), cudaMemcpyHostToDevice );
+  cudaMemcpy( c_d, c_h, size * sizeof(int), cudaMemcpyHostToDevice );
+  
+  /* Define grid dimensions */
+  dim3 grid_dim(n_blocks);
+  dim3 block_dim(n_threads);
+
+  /* Call kernel */
+  vector_addition_gpu<<<grid_dim, block_dim>>>( a_d, b_d, c_d, size);
+
+  cudaMemcpy( c_h, c_d, size * sizeof(int), cudaMemcpyDeviceToHost );
+
+  /* Make sure GPU work is done */
+  cudaDeviceSynchronize();
+
+  check_elements(22, c_h, size);
+
+  cudaFree( a_d );
+  cudaFree( b_d );
+  cudaFree( c_d );
+
+  /* free host memory */
+  delete [] a_h;
+  delete [] b_h;
+  delete [] c_h;
+
+  
+  return 0;
+}
